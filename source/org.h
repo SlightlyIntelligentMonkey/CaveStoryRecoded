f30480cf14e53274bb3caaab705431711020fa9c
GabrielRavier <gabravier@gmail.com>
2018-08-21 12:16:33 +0200

    Added requirements for org implementation in org.h

--- source/org.h
+++ source/org.h
@@ -1,6 +1,30 @@
 #pragma once
 #include "common.h"
 
+/**
+
+Org implementation requirements (names aren't strict) :
+
+/// Loads a music from a given path
+void loadOrg(const char *path);
+
+/// Returns the current song's position
+int32_t getOrgPosition();
+
+/// The returned position from getOrgPosition is usable here. Position 0 is the start of the song
+void setOrgPosition(int32_t position);
+
+/// Playback starts when this function is called
+void startOrgPlayback();
+
+/// Playback ends when this function is called
+void stopOrgPlayback();
+
+/// Is set to false on loadOrg and setOrgPosition. Volume starts decreasing when it is set to true
+bool musicFadeFlag;
+
+*/
+
 struct NOTE
 {
 	uint32_t pos;
@@ -18,7 +42,7 @@ struct TRACK
 
 	uint16_t amountNotes;
 	NOTE *note;
-	
+
 	uint32_t samples;
 };
 
@@ -38,4 +62,50 @@ struct ORG
 
 	uint32_t pos;
 	uint32_t samples;
+};
+
+constexpr const char *songNames[42]
+{
+    "xxxx",
+    "wanpaku"
+    "anzen",
+    "gameover",
+    "gravity",
+    "weed",
+    "mdown2",
+    "fireeye",
+    "vivi",
+    "mura",
+    "fanfale1",
+    "ginsuke",
+    "cemetary",
+    "plant",
+    "kodou",
+    "fanfale3",
+    "fanfale2",
+    "dr",
+    "escape",
+    "jenka",
+    "maze",
+    "access",
+    "ironh",
+    "grand",
+    "curly",
+    "oside",
+    "requiem",
+    "wanpak2",
+    "quiet",
+    "lastcave",
+    "balcony",
+    "lastbtl",
+    "lastbtl3",
+    "ending",
+    "zonbie",
+    "bdown",
+    "hell",
+    "jenka2",
+    "marine",
+    "ballos",
+    "toroko",
+    "white",
 };
