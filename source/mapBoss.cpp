9bcc1b1222d1938181e3812604ad72642e4d8cd5
SlightlyIntelligentMonkey <35209503+SlightlyIntelligentMonkey@users.noreply.github.com>
2018-09-08 17:01:02 -0400

    started map boss

---
+++ source/mapBoss.cpp
@@ -0,0 +1,23 @@
+#include "mapBoss.h"
+
+npc boss;
+
+typedef void(*BOSSFUNCTION)(void);
+BOSSFUNCTION gpBossFuncTbl[10] =
+{
+  &ActBossChar_0,
+  &ActBossChar_Omega,
+  &ActBossChar_Frog,
+  &ActBossChar_MonstX,
+  &ActBossChar_Core,
+  &ActBossChar_Ironhead,
+  &ActBossChar_Twin,
+  &ActBossChar_Undead,
+  &ActBossChar_Press,
+  &ActBossChar_Ballos
+}; // idb
+
+void ActBossChar_0() 
+{
+	return; 
+}
---
+++ source/mapBoss.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "npc.h"
+
+void ActBossChar_0();
+void ActBossChar_Omega();
+void ActBossChar_Frog();
+void ActBossChar_MonstX();
+void ActBossChar_Core();
+void ActBossChar_Ironhead();
+void ActBossChar_Twin();
+void ActBossChar_Undead();
+void ActBossChar_Press();
+void ActBossChar_Ballos();
