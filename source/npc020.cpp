909cc0536c2cc288e13ada7b36992bf5426463f8
GreenDev <32424793+ThatOneGreenGuy@users.noreply.github.com>
2018-09-07 20:45:39 -0400

    probably gabriel

--- source/npc020.cpp
+++ source/npc020.cpp
@@ -89,12 +89,9 @@ void npcAct025(npc *NPC) //egg corridor lift thing
 		NPC->act_no = 1;
 		NPC->ani_no = 0;
 		NPC->ani_wait = 0;
-		NPC->x += 4096;
-		// Fallthrough
+		NPC->x += 0x1000;
+//Fallthrough
 	case 1:
-	case 3:
-	case 5:
-	case 7:
 		if (++NPC->act_wait > 150)
 		{
 			NPC->act_wait = 0;
@@ -102,20 +99,74 @@ void npcAct025(npc *NPC) //egg corridor lift thing
 		}
 		break;
 	case 2:
-	case 4:
-	case 6:
-	case 8:
 		if (++NPC->act_wait > 64)
 		{
 			NPC->act_wait = 0;
 			++NPC->act_no;
 		}
 		else
-			NPC->y -= 512;
+		{
+			NPC->y -= 0x200;
+		}
+		break;
+	case 3:
+		if (++NPC->act_wait > 150)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		break;
+	case 4:
+		if (++NPC->act_wait > 64)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		else
+		{
+			NPC->y -= 0x200;
+		}
+		break;
+	case 5:
+		if (++NPC->act_wait > 150)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		break;
+	case 6:
+		if (++NPC->act_wait > 64)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		else
+		{
+			NPC->y += 512;
+		}
+		break;
+	case 7:
+		if (++NPC->act_wait > 150)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		break;
+	case 8:
+		if (++NPC->act_wait > 64)
+		{
+			NPC->act_wait = 0;
+			NPC->act_no = 1;
+		}
+		else
+		{
+			NPC->y += 0x200;
+		}
 		break;
 	default:
 		break;
 	}
+
 	if (NPC->act_no <= 8 && (1 << NPC->act_no) & 0x154)
 	{
 		if (++NPC->ani_wait > 1)
