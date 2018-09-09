7469e033c20cbb03f82a321ed50ea30de3456690
GreenDev <32424793+ThatOneGreenGuy@users.noreply.github.com>
2018-09-09 18:36:22 -0400

    org positions are now floats

--- source/game.cpp
+++ source/game.cpp
@@ -229,10 +229,12 @@ void debugFunction()
 		string debugStr2 = "There are " + to_string(bullets.size()) + " bullet slots.";
 		string debugStr3 = "There are " + to_string(carets.size()) + " caret slots.";
 		string debugStr4 = "There are " + to_string(valueviews.size()) + " valueview slots";
+		string debugStr5 = "Currently loaded boss is " + to_string(boss[0].code_char);
 		drawString(8, screenHeight - 12, debugStr1.c_str());
 		drawString(8, screenHeight - 24, debugStr2.c_str());
 		drawString(8, screenHeight - 36, debugStr3.c_str());
 		drawString(8, screenHeight - 48, debugStr4.c_str());
+		drawString(8, screenHeight - 60, debugStr5.c_str());
 	}
 }
 
