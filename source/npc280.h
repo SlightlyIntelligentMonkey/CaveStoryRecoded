11bfea0216f6de3afa0e0e9e6eba05866b293fd3
GabrielRavier <gabravier@gmail.com>
2018-10-12 14:07:57 +0200

    Some new NPCs and clipboard handling for the tsc debug
    Signed-off-by: GabrielRavier <gabravier@gmail.com>

--- source/npc280.h
+++ source/npc280.h
@@ -4,6 +4,8 @@
 
 void npcAct292(npc *NPC);
 
+void npcAct295(npc *NPC);
+void npcAct296(npc *NPC);
 void npcAct297(npc *NPC);
 void npcAct298(npc *NPC);
 void npcAct299(npc *NPC);
--- source/npc340.h
+++ source/npc340.h
@@ -6,4 +6,7 @@ void npcAct347(npc *NPC);
 
 void npcAct349(npc *NPC);
 
+void npcAct354(npc *NPC);
+void npcAct355(npc *NPC);
+
 void npcAct359(npc *NPC);
