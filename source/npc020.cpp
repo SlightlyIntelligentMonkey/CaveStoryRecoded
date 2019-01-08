da48fa74593efa5c44f5a94ac7fd6536467648a9
Clownacy <Clownacy@users.noreply.github.com>
2019-01-08 19:50:16 +0000

    Fixed NPC25 (Egg Corridor missile room elevator)
    Also made the code closer to the Windows original

--- source/npc020.cpp
+++ source/npc020.cpp
@@ -202,9 +202,6 @@ void npcAct025(npc *NPC) //egg corridor lift thing
 		NPC->x += tilesToUnits(0.5);
 //Fallthrough
 	case 1:
-    case 3:
-    case 5:
-    case 7:
 		if (++NPC->act_wait > 150)
 		{
 			NPC->act_wait = 0;
@@ -212,9 +209,6 @@ void npcAct025(npc *NPC) //egg corridor lift thing
 		}
 		break;
 	case 2:
-    case 4:
-    case 6:
-    case 8:
 		if (++NPC->act_wait > 64)
 		{
 			NPC->act_wait = 0;
@@ -223,12 +217,64 @@ void npcAct025(npc *NPC) //egg corridor lift thing
 		else
 			NPC->y -= 0x200;
 		break;
+	case 3:
+		if (++NPC->act_wait > 150)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		break;
+	case 4:
+		if (++NPC->act_wait > 64)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		else
+			NPC->y -= 0x200;
+		break;
+	case 5:
+		if (++NPC->act_wait > 150)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		break;
+	case 6:
+		if (++NPC->act_wait > 64)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		else
+			NPC->y += 0x200;
+		break;
+	case 7:
+		if (++NPC->act_wait > 150)
+		{
+			NPC->act_wait = 0;
+			++NPC->act_no;
+		}
+		break;
+	case 8:
+		if (++NPC->act_wait > 64)
+		{
+			NPC->act_wait = 0;
+			NPC->act_no = 1;
+		}
+		else
+			NPC->y += 0x200;
+		break;
 	default:
 		break;
 	}
 
-	if (NPC->act_no <= 8 && (1 << NPC->act_no) & 0x154)
+	switch (NPC->act_no)// <= 8 && (1 << NPC->act_no) & 0x154)
 	{
+	case 2:
+	case 4:
+	case 6:
+	case 8:
 		if (++NPC->ani_wait > 1)
 		{
 			NPC->ani_wait = 0;
@@ -236,6 +282,9 @@ void npcAct025(npc *NPC) //egg corridor lift thing
 		}
 		if (NPC->ani_no > 1)
 			NPC->ani_no = 0;
+		break;
+	default:
+		break;
 	}
 
 	NPC->doRects({256, 64 + (16 * NPC->ani_no), 288, 80 + (16 * NPC->ani_no)});
